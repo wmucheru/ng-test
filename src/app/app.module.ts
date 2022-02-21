fae9d7c8216541413adb5390b877410d6a76d98f
William Mucheru <willyk99@gmail.com>
2022-02-22 00:31:26 +0300

    Test heroes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+
+import { DataTablesModule } from "angular-datatables";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    // Datatables
+    DataTablesModule,
+
+    // Material UI
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
